f2adb8b4fafc0df1ce156d88c6f95efbc53d2fa9
Mabel Felix <demouser@Mabel.local>
2016-07-25 10:49:36 -0700

    second commit - check main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 # don't forget to comment y'all
 
 import webapp2
-import jinja2
-import os
+#import jinja2
+#import os
 import json
+from google.appengine.api import urlfetch
+import urllib2
+from random import randint
 # from 'file name' import 'class'
 # from 'api' import 'class'
 
@@ -20,6 +23,27 @@ class MainHandler(webapp2.RequestHandler):
     def get(self):
         self.response.write('Hello world!')
 
+class RandomHandler(webapp2.RequestHandler):
+    def get(self):
+        data_source = urlfetch.fetch(self.movie_search())
+        results = json.loads(data_source.content)
+
+        genre = results['Genre']
+
+        self.response.write(genre)
+
+    def movie_search(self):
+        #please find a way to make the random functional (adding leading zeros)
+        #r creates a random imdb id from 0 to 2000000 which is entered into the
+        #url for the api
+        #movie_search returns the full url with the random id
+        r = randint(0,2000000)
+        full_url = "http://www.omdbapi.com/?i=tt" + str(r)
+
+        return full_url
+
+
+
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', RandomHandler)
 ], debug=True)
