59b129224e4518364781b19fba354dc029c81e36
Corine Lu <demouser@Sloth.local>
2016-07-25 10:01:34 -0700

    More template changes to the main.py

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ import jinja2
 import os
 import json
 # from 'file name' import 'class'
+# from 'api' import 'class'
+
+# jinja_environment = jinja2.Environment(
+#   loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))# this little bit sets jinja's relative directory to match the directory name(dirname) of the current __file__, in this case, helloworld.py
+# env = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'))
 
 # this is to link to the html file
 # make sure to change 'search_template' to something relevant
