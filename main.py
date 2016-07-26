37180286bc23b8c7615e2a00ffc61f3585692cf2
Corine <clu1438@gmail.com>
2016-07-26 15:40:40 -0700

    Completed random handle

--- main.py
+++ main.py
@@ -34,24 +34,30 @@ class RandomHandler(webapp2.RequestHandler):
         # r creates a random imdb id from 0 to 2000000 which is entered into the
         # url for the api
         # function returns the full url with the random id
-        randomInt = str(randint(0,2000000))
-        full_url = "http://www.omdbapi.com/?i=tt" + randomInt.zfill(7)
+        pageNumber = str(randint(1,1000))
+        firstUrlPart = 'https://api.themoviedb.org/3/discover/movie?'
+        page = 'page='
+        apiKey = '&api_key=7f2e8836857048a3c77885647f9c0f47'
+        full_url = firstUrlPart + page + pageNumber + apiKey
 
         data_source = urlfetch.fetch(full_url)
-        results = json.loads(data_source.content)
+        movies = json.loads(data_source.content)
 
         # refreshes page if not a movie or and Adult movie
-        if not results['Type'] == 'movie' or results['Genre'] == 'Adult': #or if hasattr(results,'Error'):
-            self.redirect("/random")
+        #if results['movie_results']['adult'] == false:
+        #    self.redirect("/random")
 
         #movieQuery = Movie.query()
         #movie = movieQuery.fetch()
 
         # prints movie information
-        self.response.write(results['Title'] + '<br>')
-        self.response.write(results['Year'] + ' - ' + results['Genre'] + '<br>')
-        if results['Plot'] != "N/A":
-            self.response.write(results['Plot'])
+        indexNumber = randint(1,19)
+        self.response.write(movies['results'][indexNumber]['title'] + '<br>')
+        self.response.write('Release Date: ' + movies['results'][indexNumber]['release_date'] + '<br>')
+        self.response.write('Popularity: ' + str(movies['results'][indexNumber]['popularity']) + '<br>')
+        self.response.write('Rating: ' + str(movies['results'][indexNumber]['vote_average']) + '<br>')
+        self.response.write(movies['results'][indexNumber]['overview'] + '<br>')
+
 
 
 app = webapp2.WSGIApplication([
