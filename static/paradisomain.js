6c7b89e495cd3f941e7e5ef98116aecbead7a07d
Adrian Mendoza <adrian24men@gmail.com>
2016-07-27 13:34:10 -0700

    Fixed recommended by genre

--- static/paradisomain.js
+++ static/paradisomain.js
@@ -6,16 +6,16 @@ function myFunction() {
 
 // Close the dropdown if the user clicks outside of it
 
-window.onmouseover = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
+// window.onmouseover = function(event) {
+//   if (!event.target.matches('.dropbtn')) {
+//
+//     var dropdowns = document.getElementsByClassName("dropdown-content");
+//     var i;
+//     for (i = 0; i < dropdowns.length; i++) {
+//       var openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains('show')) {
+//         openDropdown.classList.remove('show');
+//       }
+//     }
+//   }
+// }
